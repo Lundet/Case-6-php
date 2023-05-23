423660b380fa979d54b8a5dccd1c757123b6bf8a
Lundet <hannes.j.lundin@gmail.com>
2023-05-23 16:23:31 +0200

    fixed the button and some margin in the label

--- app/public/logout.php
+++ app/public/logout.php
@@ -10,12 +10,12 @@ session_destroy();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/global.css">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Logga ut</title>
 </head>
 <body>
     
-    Du är nu utloggad - tillbaka till <a href="login.php">att kunna logga in</a>;
+    Du är nu utloggad - tillbaka till <a href="login.php">att kunna logga in</a>
 
 </body>
 </html>
