e7885278e55ff624acfeb59ae6cf6529b5730a14
Lundet <hannes.j.lundin@gmail.com>
2023-05-26 17:42:51 +0200

    fixed styling

--- app/public/index.php
+++ app/public/index.php
@@ -18,8 +18,7 @@ create_book_table($pdo);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/index.css">
-    
-    <title><?php echo $title; ?></title>
+      
 </head>
 <body>
 
