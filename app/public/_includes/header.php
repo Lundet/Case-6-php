bdbbb3fce410bc18b4d883a73a3ca925464e8259
Lundet <hannes.j.lundin@gmail.com>
2023-05-30 15:54:26 +0200

    Added a file for all reviews and change the footer and header

--- app/public/_includes/header.php
+++ app/public/_includes/header.php
@@ -1,21 +1,23 @@
 <header>
+    <!-- If the username is available, display it -->
+    <?php if (isset($_SESSION['username'])): ?>
+        <p>Inloggad som
+            <?= $_SESSION['username'] ?>
+        </p>
+    <?php endif; ?>
     <nav>
-    <!-- Om användare inte är inloggad -->
-    <?php if (!isset($_SESSION['username'])) : ?>
-        <a href="index.php">Start</a> | <a href="login.php">Logga in</a> | <a href="register.php">Registrera</a>
-        
-    <!-- Om användare är inloggad -->
-    <?php else : ?>
-        <a href="review.php">Recension</a> | <a href="my-reviews.php">Mina recensioner</a> | <a href="logout.php">Logga ut</a>
-    <?php endif; ?>
+        <!-- Om användare inte är inloggad -->
+        <?php if (!isset($_SESSION['username'])): ?>
+            <a href="index.php">Start</a>  <a href="login.php">Logga in</a>  <a href="register.php">Registrera</a>
 
-    
-</nav>
-<!-- Om användarenamn finns, skriv ut -->
-<?php if (isset($_SESSION['username'])) : ?>
-    <p >Inloggad som <?= $_SESSION['username'] ?></p>
-    <?php endif; ?>
+            <!-- Om användarenamn finns, skriv ut -->
+        <?php else: ?>
+            <a href="my-reviews.php">Mina recensioner</a>  <a href="all-reviews.php">Alla Recensioner</a>  <a
+                href="logout.php">Logga ut</a>
+        <?php endif; ?>
+    </nav>
+
+
 </header>
-
 
 <hr>
